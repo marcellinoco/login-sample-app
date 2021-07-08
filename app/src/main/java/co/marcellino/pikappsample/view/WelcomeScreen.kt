693ccb1abae9e249d955f575f3892c0b721cca9b
MarcellinoCO <marcellinocovara@gmail.com>
2021-07-08 22:19:49 +0700

    Implement welcome screen

--- app/src/main/java/co/marcellino/pikappsample/view/WelcomeScreen.kt
+++ app/src/main/java/co/marcellino/pikappsample/view/WelcomeScreen.kt
@@ -1,12 +1,24 @@
 package co.marcellino.pikappsample.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import co.marcellino.pikappsample.model.User
 
 @Composable
 fun WelcomeScreen(
     user: User
 ) {
-    Text("Hai ${user.email}")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Welcome,")
+        Text(user.email)
+    }
 }
