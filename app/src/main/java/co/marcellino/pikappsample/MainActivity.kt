6b3a260016bce12d49b26f277aa47633a0d3bf99
MarcellinoCO <marcellinocovara@gmail.com>
2021-07-08 23:09:16 +0700

    Pop navigation stack on screen change and implement snack bar

--- app/src/main/java/co/marcellino/pikappsample/MainActivity.kt
+++ app/src/main/java/co/marcellino/pikappsample/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material.Snackbar
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,6 +20,7 @@ import co.marcellino.pikappsample.view.SignInScreen
 import co.marcellino.pikappsample.view.SignUpScreen
 import co.marcellino.pikappsample.view.WelcomeScreen
 import co.marcellino.pikappsample.viewmodel.UserViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -37,6 +40,11 @@ fun BaseAppScreen(userViewModel: UserViewModel) {
     val navController = rememberNavController()
     val composableScope = rememberCoroutineScope()
 
+    val focusManager = LocalFocusManager.current
+
+    var isSnackBarVisible by remember { mutableStateOf(false) }
+    var snackBarText by remember { mutableStateOf("") }
+
     MaterialTheme {
         NavHost(
             navController = navController,
@@ -48,14 +56,26 @@ fun BaseAppScreen(userViewModel: UserViewModel) {
                 SignInScreen(
                     onSignIn = { email, password ->
                         composableScope.launch {
+                            focusManager.clearFocus()
+
+                            isSnackBarVisible = true
+                            snackBarText = "Signing in, loading..."
                             Log.d("SignInScreen", "Signing in, loading...")
 
                             if (userViewModel.attemptSignIn(email = email, password = password)) {
+                                isSnackBarVisible = false
                                 Log.d("SignInScreen", "User signed in!")
-                                navController.navigate("Welcome")
+
+                                navController.navigate("Welcome") {
+                                    popUpTo("SignIn") { inclusive = true }
+                                }
                             } else {
+                                snackBarText = "Failed to sign in!"
                                 Log.d("SignInScreen", "Failed to sign in!")
                             }
+
+                            delay(2000)
+                            isSnackBarVisible = false
                         }
                     },
                     navigateSignUp = { navController.navigate("SignUp") }
@@ -63,25 +83,37 @@ fun BaseAppScreen(userViewModel: UserViewModel) {
             }
 
             composable("SignUp") {
-                SignUpScreen(
-                    onSignUp = { email, password ->
-                        composableScope.launch {
-                            Log.d("SignUpScreen", "Signing up, loading...")
+                SignUpScreen(onSignUp = { email, password ->
+                    composableScope.launch {
+                        focusManager.clearFocus()
 
-                            if (userViewModel.attemptSignUp(email = email, password = password)) {
-                                Log.d("SignUpScreen", "User created!")
-                                navController.navigate("SignIn")
-                            } else {
-                                Log.d("SignUpScreen", "Failed to sign up!")
+                        isSnackBarVisible = true
+                        snackBarText = "Signing up, loading..."
+                        Log.d("SignUpScreen", "Signing up, loading...")
+
+                        if (userViewModel.attemptSignUp(email = email, password = password)) {
+                            isSnackBarVisible = false
+                            Log.d("SignUpScreen", "User created!")
+
+                            navController.navigate("SignIn") {
+                                popUpTo("SignUp") { inclusive = true }
                             }
+                        } else {
+                            snackBarText = "Failed to sign up!"
+                            Log.d("SignUpScreen", "Failed to sign up!")
                         }
+
+                        delay(2000)
+                        isSnackBarVisible = false
                     }
-                )
+                })
             }
 
             composable("Welcome") {
                 WelcomeScreen(user = userViewModel.currentUser!!)
             }
         }
+
+        if (isSnackBarVisible) Snackbar { Text(snackBarText) }
     }
 }
